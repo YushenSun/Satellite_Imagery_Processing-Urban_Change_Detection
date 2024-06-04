7c63623bca401287789758180353ac7ef4637b47
Yushen Sun <sun.yushen@gmail.com>
2024-06-04 23:20:41 +0200

    move tifs into a new folder

--- list_move.py
+++ list_move.py
@@ -1,10 +1,12 @@
 import os
+import shutil
 from glob import glob
 
 def list_tifs_by_size(folder):
-    # Define possible folder names based on provided patterns
+    # Define possible folder names
     row_offsets = [0, 104, 216]
     column_offsets = [0, 104, 216, 328, 440, 552, 664, 882]
+
 
     # Construct the full paths for all possible subfolders
     subfolders = [f"row_offset_{row}_column_offset_{col}" for row in row_offsets for col in column_offsets]
@@ -22,6 +24,24 @@ def list_tifs_by_size(folder):
     for tif in tifs:
         print(f"{os.path.basename(tif)}: {os.path.getsize(tif)} bytes")
 
+    return tifs
+
+def move_tifs_to_folder(tifs, dest_folder):
+    # check whether the destination folder exists already, if not, create it
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)
+
+    # move tifs to the destination folder
+    for tif in tifs:
+        shutil.move(tif, os.path.join(dest_folder, os.path.basename(tif)))
+
+
 if __name__ == "__main__":
-    folder = "imagery\patch_size_128_patch_overlap_16\patches"
-    list_tifs_by_size(folder)
+    src_folder = "imagery/patch_size_128_patch_overlap_16/patches"
+    dest_folder = "all_tifs"
+
+    print("Listing TIFF files by size...")
+    tifs = list_tifs_by_size(src_folder)
+
+    print("\nMoving TIFF files to 'all_tifs' folder...")
+    move_tifs_to_folder(tifs, dest_folder)
